9b50ec27746cc3aeaac5529d24abc84f333becbe
SkyKnigth <129749991+SkyKnigth@users.noreply.github.com>
2024-02-12 15:05:51 +0100

    input jscript

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,75 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const dotsContainer = document.querySelector(".dots")
+console.log(dotsContainer);
+const arrowLeft = document.querySelector(".arrow_left")
+console.log(arrowLeft)
+const arrowRight = document.querySelector(".arrow_right")
+console.log(arrowRight)
+const p = document.querySelector("#banner p")
+console.log(p)
+const image = document.querySelector(".banner-img")
+
+let currentImageIndex = 0
+
+function dotsCreation(){
+	for(let i =0; i<slides.length;i++ ){
+		const slideDot = document.createElement("span")
+		slideDot.classList.add("dot")
+		if(i===0){
+			slideDot.classList.add("dot_selected") 
+		}
+		dotsContainer.appendChild(slideDot)
+	}
+
+}
+dotsCreation()
+const dots= document.querySelectorAll(".dot")
+// evenement clic souris flêche
+
+arrowLeft.addEventListener("click",function(event){
+	console.log(event)
+	showPreviousImage()
+})
+
+arrowRight.addEventListener("click",function(event){
+	console.log(event)
+	showNextImage()
+})
+
+function showImage(currentIndex){
+	if (currentIndex < 0 || currentIndex>= slides.length) return
+	console.log (dots)
+	console.log(currentIndex)
+	const src = "./assets/images/slideshow/"+ slides[currentIndex].image
+	image.src=src
+
+	p.innerHTML = slides[currentIndex].tagLine
+
+	dots.forEach((dot, index) => {
+		if (currentIndex === index) {
+			//image.style.display = 'block';
+			dot.classList.add('dot_selected');
+		} else {
+			//image.style.display = 'none';
+			dot.classList.remove('dot_selected');
+		}
+	});
+}
+
+function showNextImage() {
+	currentImageIndex = (currentImageIndex + 1) % slides.length;
+	showImage(currentImageIndex);
+}
+
+
+function showPreviousImage() {
+	console.log(currentImageIndex)
+	currentImageIndex = (currentImageIndex - 1 + slides.length) % slides.length
+	console.log(currentImageIndex)
+	showImage(currentImageIndex);
+	
+}
+
